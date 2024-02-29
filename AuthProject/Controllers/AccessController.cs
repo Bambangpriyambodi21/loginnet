00453b206172475304e0590a01c472bb1e584474
Bambang Priyambodo <bambangpriyambodi21@gmail.com>
2024-02-29 11:23:13 +0700

    [First commit]

---
+++ AuthProject/Controllers/AccessController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using AuthProject.Models;
+
+namespace AuthProject.Controllers
+{
+    public class AccessController : Controller
+    {
+        public IActionResult Login()
+        {
+            ClaimsPrincipal claimsUser = HttpContext.User;
+            if (claimsUser.Identity.IsAuthenticated)
+                return RedirectToAction("Index", "Home");
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(VMLogin modelLogin)
+        {
+            if (modelLogin.Email=="user@example.com" && modelLogin.PassWord == "123")
+            {
+                List<Claim> claims = new List<Claim>()
+                {
+                    new Claim(ClaimTypes.NameIdentifier, modelLogin.Email),
+                    new Claim("OtherProperties","Example Role")
+                };
+
+                ClaimsIdentity identity = new ClaimsIdentity(claims,
+                    CookieAuthenticationDefaults.AuthenticationScheme);
+
+                AuthenticationProperties properties = new AuthenticationProperties()
+                {
+                    AllowRefresh = true,
+                    IsPersistent = modelLogin.KeepLoggedIn
+                };
+
+                await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme,
+                    new ClaimsPrincipal(identity), properties);
+
+                return RedirectToAction("index", "home");
+            }
+
+            ViewData["ValidateMessage"] = "user not found";
+            return View();
+        }
+    }
+}
